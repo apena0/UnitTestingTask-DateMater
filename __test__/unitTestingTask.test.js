d573df04b9c3c779f8611c6d2bc538e69fd1cccf
Arlette Pena <apena@griddynamics.com>
2024-02-01 17:06:59 -0600

    Correction in unit tests

--- __test__/unitTestingTask.test.js
+++ __test__/unitTestingTask.test.js
@@ -1,154 +1,88 @@
-describe('Unit Testing Task', () => {
-  const unitTestingTask = require('../unitTestingTask');
-  const timezoneMock = require('timezone-mock');
-  const preloadedDate = '1995-09-05T03:05:03.007';
-
-  it('should return 4-digit year', () => {
-    expect(unitTestingTask('YYYY', preloadedDate)).toBe('1995');
-  });
-
-  it('should return last 2 digit year', () => {
-    expect(unitTestingTask('YY', preloadedDate)).toBe('95');
-  });
-
-  it('should return full name of month', () => {
-    expect(unitTestingTask('MMMM', preloadedDate)).toBe('September');
-  });
-
-  it('should return short name of month', () => {
-    expect(unitTestingTask('MMM', preloadedDate)).toBe('Sep');
-  });
-
-  it('should return ISO8601 compatible number of month', () => {
-    expect(unitTestingTask('MM', preloadedDate)).toBe('09');
-  });
-
-  it('should return number of the month in year without zero-padding', () => {
-    expect(unitTestingTask('M', preloadedDate)).toBe('9');
-  });
-  it('should return full name of day', () => {
-    expect(unitTestingTask('DDD', preloadedDate)).toBe('Tuesday');
-  });
-
-  it('should return shot name of day', () => {
-    expect(unitTestingTask('DD', preloadedDate)).toBe('Tue');
-  });
-
-  it('should return min name of day', () => {
-    expect(unitTestingTask('D', preloadedDate)).toBe('Tu');
-  });
-
-  it('should return zero-padded number of day in month', () => {
-    expect(unitTestingTask('dd', preloadedDate)).toBe('05');
-  });
-
-  it('should return number of day in month', () => {
-    expect(unitTestingTask('d', preloadedDate)).toBe('5');
-  });
-
-  it('should return zero-padded hour in 24-hr format', () => {
-    expect(unitTestingTask('HH', preloadedDate)).toBe('03');
-  });
-
-  it('should return hour in 24-hr format', () => {
-    expect(unitTestingTask('H', preloadedDate)).toBe('3');
-  });
-
-  it('should return zero-padded hour in 12-hour format', () => {
-    expect(unitTestingTask('hh', '1995-09-05T13:15:30')).toBe('01');
-  });
-
-  it('should return zero-padded hour in 12-hour format in case is 12pm', () => {
-    expect(unitTestingTask('hh', '1995-09-05T12:15:30')).toBe('12');
-  });
-
-  it('should return hour in 12-hr format in case is 12pm', () => {
-    expect(unitTestingTask('h', '1995-09-05T12:15:30')).toBe('12');
-  });
-
-  it('should return hour in 12-hr format', () => {
-    expect(unitTestingTask('h', '1995-09-05T13:15:30')).toBe('1');
-  });
-
-  it('should return zero-padded minutes', () => {
-    expect(unitTestingTask('mm', preloadedDate)).toBe('05');
-  });
-
-  it('should return minutes', () => {
-    expect(unitTestingTask('m', preloadedDate)).toBe('5');
-  });
-
-  it('should return zero-padded seconds', () =>
-    expect(unitTestingTask('ss', preloadedDate)).toBe('03'));
-
-  it('should return seconds', () => {
-    expect(unitTestingTask('s', preloadedDate)).toBe('3');
-  });
-
-  it('should return zero-padded milliseconds', () => {
-    expect(unitTestingTask('ff', preloadedDate)).toBe('007');
-  });
-
-  it('should return miliseconds', () => {
-    expect(unitTestingTask('f', preloadedDate)).toBe('7');
-  });
-
-  it('should return AM/PM', () => {
-    expect(unitTestingTask('A', preloadedDate)).toBe('AM');
-  });
-
-  it('should return am/pm', () => {
-    expect(unitTestingTask('a', preloadedDate)).toBe('am');
-  });
-
-  it('should return AM/PM', () => {
-    expect(unitTestingTask('A', '1995-09-05T13:15:30')).toBe('PM');
-  });
-
-  it('should return am/pm', () => {
-    expect(unitTestingTask('a', '1995-09-05T13:15:30')).toBe('pm');
-  });
-
-  it('should return time-zone in ISO8601-compatible basic format', () => {
-    timezoneMock.register('Etc/GMT+5');
-    expect(unitTestingTask('ZZ', preloadedDate)).toBe('-0500');
-    timezoneMock.unregister();
-  });
-
-  it('should return time-zone in ISO8601-compatible basic format plus hours', () => {
-    timezoneMock.register('Europe/London');
-    expect(unitTestingTask('ZZ', preloadedDate)).toBe('+0100');
-    timezoneMock.unregister();
-  });
-
-  it('should return time-zone in ISO8601-compatible extended format', () => {
-    timezoneMock.register('UTC');
-    expect(unitTestingTask('Z', preloadedDate)).toBe('+00:00');
-    timezoneMock.unregister();
-  });
-
-  it('should throw an error if fomat is not a string', () => {
-    expect(() => unitTestingTask(5, preloadedDate)).toThrow(TypeError);
-  });
-
-  it('should throw an error if fomat is not a string', () => {
-    expect(() => unitTestingTask('YYYY', true)).toThrow(TypeError);
+const unitTestingTask = require('../unitTestingTask');
+const timezoneMock = require('timezone-mock');
+const preloadedDate = '1995-09-05T03:05:03.007';
+const preloadedDatePm = '1995-09-05T12:15:30';
+describe('Tests for preloaded tokens ', () => {
+  test.each([
+    { token: 'YYYY', date: preloadedDate, expec: '1995', desc: '4-digit year' },
+    { token: 'YY', date: preloadedDate, expec: '95', desc: 'last 2 digit year'},
+    { token: 'MMMM', date: preloadedDate, expec: 'September', desc: 'full name of month'},
+    { token: 'MMM', date: preloadedDate, expec: 'Sep', desc: 'short name of month'},
+    { token: 'M', date: preloadedDate, expec: '9', desc: ' number of the month in year without zero-padding'},
+    { token: 'DDD', date: preloadedDate, expec: 'Tuesday', desc: 'full name of day'},
+    { token: 'DD', date: preloadedDate, expec: 'Tue', desc: 'short name of day'},
+    { token: 'D', date: preloadedDate, expec: 'Tu', desc: 'min name of day'},
+    { token: 'D', date: preloadedDate, expec: 'Tu', desc: 'min name of day'},
+    { token: 'dd', date: preloadedDate, expec: '05', desc: 'zero-padded number of day in month'},
+    { token: 'd', date: preloadedDate, expec: '5', desc: 'number of day in month'},
+    { token: 'HH', date: preloadedDate, expec: '03', desc: 'zero-padded hour in 24-hr format'},
+    { token: 'H', date: preloadedDate, expec: '3', desc: 'hour in 24-hr format'},
+    { token: 'hh', date: preloadedDate, expec: '03', desc: 'zero-padded hour in 12-hour format'},
+    { token: 'hh', date: preloadedDatePm, expec: '12', desc: 'zero-padded hour in 12-hour format in case 12pm'},
+    { token: 'h', date: preloadedDate, expec: '3', desc: 'hour in 12-hr format'},
+    { token: 'h', date: preloadedDatePm, expec: '12', desc: 'hour in 12-hr format in case 12pm'},
+    { token: 'mm', date: preloadedDate, expec: '05', desc: 'zero-padded minutes'},
+    { token: 'm', date: preloadedDate, expec: '5', desc: 'return minutes' },
+    { token: 'ss', date: preloadedDate, expec: '03', desc: 'zero-padded seconds'},
+    { token: 's', date: preloadedDate, expec: '3', desc: 'seconds' },
+    { token: 'ff', date: preloadedDate, expec: '007', desc: 'zero-padded milliseconds',},
+    { token: 'f', date: preloadedDate, expec: '7', desc: 'milliseconds' },
+    { token: 'A', date: preloadedDate, expec: 'AM', desc: 'AM' },
+    { token: 'a', date: preloadedDate, expec: 'am', desc: 'am' },
+    { token: 'A', date: preloadedDatePm, expec: 'PM', desc: 'PM' },
+    { token: 'a', date: preloadedDatePm, expec: 'pm', desc: 'pm' },
+  ])('$token should return $desc', ({ token, date, expec, desc }) => {
+    expect(unitTestingTask(token, date)).toBe(expec);
   });
 
   it('Should run noConflict function and return the same unit testing task function', () => {
     expect(unitTestingTask.noConflict()).toBeInstanceOf(Function);
   });
 
+  describe('Time-zone related tests', () => {
+    it('should return time-zone in ISO8601-compatible basic format', () => {
+      timezoneMock.register('Etc/GMT+5');
+      expect(unitTestingTask('ZZ', preloadedDate)).toBe('-0500');
+      timezoneMock.unregister();
+    });
+
+    it('should return time-zone in ISO8601-compatible basic format plus hours', () => {
+      timezoneMock.register('Europe/London');
+      expect(unitTestingTask('ZZ', preloadedDate)).toBe('+0100');
+      timezoneMock.unregister();
+    });
+
+    it('should return time-zone in ISO8601-compatible extended format', () => {
+      timezoneMock.register('UTC');
+      expect(unitTestingTask('Z', preloadedDate)).toBe('+00:00');
+      timezoneMock.unregister();
+    });
+  });
+
+  describe('Errors tests', () => {
+
+    it('should throw an error if fomat is not a string', () => {
+      expect(() => unitTestingTask(5, preloadedDate)).toThrow(TypeError);
+    });
+
+    it('should throw an error if fomat is not a string', () => {
+      expect(() => unitTestingTask('YYYY', true)).toThrow(TypeError);
+    });
+  });
+
+  
+
   describe('Languages tests', () => {
+
     it('If language is not specified, should return the default language', () => {
       expect(unitTestingTask.lang()).toBe('en');
     });
 
-    it('If language is specified, should try tu run module and find it', () => {
+    it('If language is specified and module is not find, should return default language ', () => {
       expect(unitTestingTask.lang('uk')).toBe('en');
     });
-
-    it('If language is specified, should try tu run module and find it', () => {
+    
+    it('If language is specified, should try to run module and find it', () => {
       jest.mock('../lang/uk.js', () => {
         return {};
       });
@@ -158,7 +92,7 @@ describe('Unit Testing Task', () => {
   });
 
   describe('Formatting tests', () => {
-    it('Should return list of formatters', () => {
+    it('Should return list of preloaded formatters', () => {
       const objectKeysArray = [
         'ISODate',
         'ISOTime',
